8079de6fec61a3dbf5d9351ef1a3cda0cc5bf92d
Bee Klimt <beeklimt@honeycomb.io>
2025-02-14 14:23:03 -0800

    feat: add a react native expo sample app (#1)
    * feat: initial example app commit
    * Update README.md
    
    Co-authored-by: Jamie Danielson <jamiedanielson@honeycomb.io>
    * Update tsconfig.json
    * Update README.md
    * Update metro.config.js
    * Update README.md
    * downgrade exporter packages to fix strange error
    
    ---------
    
    Co-authored-by: Jamie Danielson <jamiedanielson@honeycomb.io>

--- README.md
+++ README.md
@@ -1,10 +1,38 @@
-# Default Community Health Files
+# React Native Honeycomb Sample App
 
-This repository contains default community health files for repositories in the Honeycomb organization and will automatically be picked up if they are not overwritten.
+This is an [Expo](https://expo.dev) project created with [`create-expo-app`](https://www.npmjs.com/package/create-expo-app) demonstrating how to integrate Honeycomb into a React Native app using OpenTelemetry SDKs.
 
-More details on this repository structure can be found here: https://docs.github.com/en/github/building-a-strong-community/creating-a-default-community-health-file
+## Setup and Run This App
 
-# {project-name}
+To use this app:
 
-<!-- OSS metadata badge - rename repo link and set status in OSSMETADATA -->
-<!-- [![OSS Lifecycle](https://img.shields.io/osslifecycle/honeycombio/{repo-name})](https://github.com/honeycombio/home/blob/main/honeycomb-oss-lifecycle-and-practices.md) -->
+1. Run `npm ci` to install dependencies.
+2. Update the `serviceName` and `honeycombKey` in [app/honeycomb.tsx](app/honeycomb.tsx).
+3. Run `npm run ios`
+
+To add Honeycomb to an existing React Native app, follow these steps:
+
+1. Add [metro.config.js](metro.config.js) to the root of your repo and enable `config.resolver.unstable_enablePackageExports`. This is required for OpenTelemetry to be able to properly import its dependencies.
+
+2. Install OpenTelemetry dependencies to your app.
+```bash
+npm install --save \
+    @opentelemetry/api \
+    @opentelemetry/api-logs@0.55.0 \
+    @opentelemetry/sdk-logs@0.55.0 \
+    @opentelemetry/sdk-metrics \
+    @opentelemetry/sdk-trace-web \
+    @opentelemetry/exporter-logs-otlp-http@0.55.0 \
+    @opentelemetry/exporter-metrics-otlp-http@0.55.0 \
+    @opentelemetry/exporter-trace-otlp-http@0.55.0
+```
+
+3. Add OpenTelemetry configuration code as found in [app/honeycomb.tsx](app/honeycomb.tsx). This code can be customized as desired.
+
+4. Update the `serviceName` and `honeycombKey` in `honeycomb.tsx` for your particular project.
+
+5. Call `configureHoneycomb();` from the top level of your app, such as in the `RootLayout` function.
+
+6. See examples in [app/(tabs)/demo.tsx](app/(tabs)/demo.tsx) for how to create manual telemetry.
+
+For more information, including how to configure auto-instrumentation for `fetch` and other libraries, see the [OpenTelemetry Demo documentation for React Native](https://opentelemetry.io/docs/demo/services/react-native-app/).
---
+++ app/honeycomb.tsx
@@ -0,0 +1,94 @@
+
+import { metrics } from '@opentelemetry/api';
+import { logs } from '@opentelemetry/api-logs';
+
+import {
+    ConsoleSpanExporter,
+    SimpleSpanProcessor,
+    WebTracerProvider,
+} from "@opentelemetry/sdk-trace-web";
+
+import {
+    ConsoleMetricExporter,
+    MeterProvider,
+    PeriodicExportingMetricReader,
+} from "@opentelemetry/sdk-metrics";
+
+import {
+    ConsoleLogRecordExporter,
+    LoggerProvider,
+    SimpleLogRecordProcessor,
+} from "@opentelemetry/sdk-logs";
+
+import { Resource } from "@opentelemetry/resources";
+
+import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-http";
+import { OTLPMetricExporter } from "@opentelemetry/exporter-metrics-otlp-http";
+import { OTLPLogExporter } from "@opentelemetry/exporter-logs-otlp-http";
+
+export default function configureHoneycomb() {
+    console.log("OpenTelemetry is initializing...");
+
+    const serviceName = "reactnative-demo";
+    const honeycombKey = "YOUR_KEY_HERE";
+    const honeycombURL = "https://api.honeycomb.io";
+
+    const resource = new Resource({
+        "service.name": serviceName,
+    });
+    const headers = {
+        "x-honeycomb-team": honeycombKey,
+        "x-honeycomb-dataset": serviceName,
+    };
+
+    // Traces
+
+    const traceProvider = new WebTracerProvider({
+        resource,
+        spanProcessors: [
+            new SimpleSpanProcessor(
+                new OTLPTraceExporter({ headers, url: `${honeycombURL}/v1/traces` })
+            ),
+            new SimpleSpanProcessor(new ConsoleSpanExporter()),
+        ],
+    });
+    traceProvider.register();
+
+    // Metrics
+
+    const metricsHeaders = {
+        "x-honeycomb-team": honeycombKey,
+        "x-honeycomb-dataset": `${serviceName}-metrics`,
+    };
+    const metricExporter = new OTLPMetricExporter({
+        headers: metricsHeaders,
+        url: `${honeycombURL}/v1/metrics`,
+    });
+    const meterProvider = new MeterProvider({
+        resource,
+        readers: [
+            new PeriodicExportingMetricReader({
+                exporter: metricExporter,
+            }),
+            new PeriodicExportingMetricReader({
+              exporter: new ConsoleMetricExporter(),
+            }),
+        ],
+    });
+    metrics.setGlobalMeterProvider(meterProvider);
+
+    // Logging
+
+    const logExporter = new OTLPLogExporter({
+        headers,
+        url: `${honeycombURL}/v1/logs`,
+    });
+    const loggerProvider = new LoggerProvider({ resource });
+    loggerProvider.addLogRecordProcessor(new SimpleLogRecordProcessor(logExporter));
+    loggerProvider.addLogRecordProcessor(new SimpleLogRecordProcessor(new ConsoleLogRecordExporter()));
+    logs.setGlobalLoggerProvider(loggerProvider);
+
+    // Example telemetry code.
+
+    console.log("OpenTelemetry is initialized.");
+}
